4045c37737908790144a53246a10e1b5b22952b4
Constanza Galía Maldonado <coco.galiam@gmail.com>
2022-11-22 21:27:19 -0300

    MiddleWares

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,21 @@
 const express = require('express');
+const { roles } = require('../constants/enum');
 const router = express.Router();
 const { getAllUsers, createUser, login, deleteUser, getOneUser, updateUser } = require('../controllers/userController');
+const { isLoggedIn, checkRole } = require('../middleware/auth');
 
 //Estructura de una ruta
 //router + tipo de pertición (GET, POST, PUT, PATH, DELETE)
 // + midleware (OPCIONAL) + CONTROLLER
-router.get('/users', getAllUsers);
-router.get('/user/:id', getOneUser);
+
+
+//router.get('/users', MIDDLEWARE ,getAllUsers);
+
+router.get('/users', isLoggedIn, getAllUsers);
+router.get('/user/:id', isLoggedIn, getOneUser);
 router.post('/createUser', createUser);
 router.post('/login', login);
-router.delete('/users/:id', deleteUser)
+router.delete('/users/:id', checkRole(roles.ADMIN), deleteUser)
 router.put('/users/:id', updateUser)
 
 

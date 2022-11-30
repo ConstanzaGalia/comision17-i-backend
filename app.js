64780e92bb62dfca5f9845b948e78c4730ec6703
Constanza Galía Maldonado <coco.galiam@gmail.com>
2022-11-29 21:42:54 -0300

    cors

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ app.use(express.json());
 //Para que nuestra APP sea un poco más segura, que no todos los dominios puedan consultar nuestros datos.
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', process.env.ORIGIN_URL);
-  res.setHeader('Access-Control-Allow-Origin', process.env.SENGRID_URL);
   res.setHeader(
     'Access-Control-Allow-Methods',
     'GET, POST, OPTIONS, PUT, PATCH, DELETE'
@@ -29,7 +28,6 @@ app.use((req, res, next) => {
   next();
 });
 app.use(cors({ origin: process.env.ORIGIN_URL }));
-app.use(cors({ origin: process.env.SENGRID_URL }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
